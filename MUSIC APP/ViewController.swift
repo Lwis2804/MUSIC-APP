51b7f2ae3b801642c37c8f53d9ecbea1f9835ef7
LUIS <luis.enrique.enc@gmail.com>
2022-10-17 12:20:01 -0500

    Initial Commit

---
+++ MUSIC APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MUSIC APP
+//
+//  Created by LUIS GONZALEZ on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
